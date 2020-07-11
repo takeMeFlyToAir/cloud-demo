a61d0cf0180c71e86e3b56642cd143757fdfe847
zhaozhirong2 <zhaozhirong@jd.com>
2020-07-11 15:22:30 +0800

    @LoadBalanced增加负载均衡功能

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # cloud-demo
+
+1、start.spring.io/actuator/info
+
+2、版本选型
+com.zzr.cloud：Hoxton.SR1
+boot：2.2.2.RELEASE
+cloud Alibaba：2.1.0.RELEASE
+java：java8
+maven：3.5
+mysql：5.7以上
+
+3、停更
+
+4、服务调用
+RestTemplate
+@LoadBalanced增加负载均衡功能
+
+5、组件
+（1）注册中心：eureka，zookeeper，consul，nacos
--- cloud-consumer-order80/src/main/java/com/zzr/cloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/zzr/cloud/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.zzr.cloud.entities.CommonResult;
 import com.zzr.cloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class OrderController {
 
-    public static final String PAYMENT_URL = "http://localhost:8001";
+    public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Autowired
     private RestTemplate restTemplate;
