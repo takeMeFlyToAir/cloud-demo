e5362e6571876639946f597aae3f8eeb84d74f06
zhaozhirong2 <zhaozhirong@jd.com>
2020-07-12 16:20:35 +0800

    HystrixDashboard 配置

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/zzr/cloud/HystrixDashboardMain9001.java
@@ -0,0 +1,21 @@
+package com.zzr.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @ClassName HystrixDashboardMain9001
+ * Description TODO
+ * Author zhaozhirong
+ * Date 2020/7/12 11:07
+ * Version 1.0
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+}
