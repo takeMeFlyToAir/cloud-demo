b90ce021e9b085bb8f946377e9d067975876214d
zhaozhirong2 <zhaozhirong@jd.com>
2020-07-13 09:52:43 +0800

    Gateway 路由demo

---
+++ cloud-gateway-gateway9527/src/main/java/com/zzr/cloud/GateWayMain9527.java
@@ -0,0 +1,22 @@
+package com.zzr.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName GateWayMain9527
+ * Description TODO
+ * Author zhaozhirong
+ * Date 2020/7/13 9:34
+ * Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+
+}
