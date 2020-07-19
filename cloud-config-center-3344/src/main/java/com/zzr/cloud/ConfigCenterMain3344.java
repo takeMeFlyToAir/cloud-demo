8c081d29148ab23cd594d844d7fd2014b9047a43
zhaozhirong2 <zhaozhirong@jd.com>
2020-07-19 10:08:39 +0800

    config 分布式配置初版

---
+++ cloud-config-center-3344/src/main/java/com/zzr/cloud/ConfigCenterMain3344.java
@@ -0,0 +1,22 @@
+package com.zzr.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @ClassName ConfigCenterMain3344
+ * Description TODO
+ * Author zhaozhirong
+ * Date 2020/7/19 10:02
+ * Version 1.0
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+
+}
